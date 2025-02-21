15602527811d1dc56f11d5334a2caa2afd27f467
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-02-21 13:52:39 +0530

    added live build logging in backend

--- apps/builder/src/index.ts
+++ apps/builder/src/index.ts
@@ -1,17 +1,17 @@
 import amqp from "amqplib";
-import { exec } from "child_process";
+import { spawn, exec } from "child_process";
 import { createClient } from "redis";
-import {QueueDeploymentMessageSchema} from 'schema'
-import {z} from 'zod'
+import { QueueDeploymentMessageSchema } from "schema";
+import { z } from "zod";
 import path from "path";
 import util from "util";
 import fs from "fs";
 import { uploadFolder } from "./s3";
 import dotenv from "dotenv";
+const execPromise = util.promisify(exec);
 dotenv.config();
 const backendURL = new URL(process.env.BACKEND_URL!);
-type MessageType = z.infer<typeof QueueDeploymentMessageSchema>
-const execPromise = util.promisify(exec);
+type MessageType = z.infer<typeof QueueDeploymentMessageSchema>;
 async function main() {
   const redis = createClient({ url: process.env.REDIS_URL });
   if (!redis.isOpen) await redis.connect();
@@ -22,48 +22,96 @@ async function main() {
     "messages",
     async (msg) => {
       try {
-        const payload = msg?.content.toString() as string
+        const payload = msg?.content.toString() as string;
         console.log("Got msg: ", payload);
-        const { domain, url, envs, outputDir,buildCmd,installCmd } = JSON.parse(payload) as MessageType
+        const { domain, url, envs, outputDir, buildCmd, installCmd } =
+          JSON.parse(payload) as MessageType;
         let envsString = ``;
-        for(let i = 0 ; i<envs.length ; i++){
+        for (let i = 0; i < envs.length; i++) {
           const key = envs[i].key;
           const value = envs[i].value;
-          envsString.concat(`-e ${key}=${value}`).concat(" ");
+          envsString.concat(`-e ${key}=${value}`).concat(" "); // gotcha point here
         }
         // check if output folder exists
         const exists = fs.existsSync(path.join(__dirname, "outputs"));
         if (!exists) fs.mkdirSync(path.join(__dirname, "outputs"));
         fs.mkdirSync(path.join(__dirname, "outputs", domain));
-        try {
-          await execPromise(
-            `docker run --name=${domain} -e GITHUB_URL=${url} -e "BUILD_CMD=${buildCmd}" -e "INSTALL_CMD=${installCmd}" ${envsString} react-builder:latest`
-          );
-        } catch (error) {
-          console.error("Docker Error: ", error);
-          redis.hSet(`deployments:${domain}`, {
-            err: JSON.stringify(error),
+        await redis.xAdd(`logs:${domain}`, "*", {
+          log: "starting build process",
+          type: "log",
+        });
+        const dockerBuildChild = spawn("docker", [
+          "run",
+          `--name=${domain}`,
+          "-e",
+          `GITHUB_URL=${url}`,
+          "-e",
+          `BUILD_CMD=${buildCmd}`,
+          "-e",
+          `INSTALL_CMD=${installCmd}`,
+          "react-builder:latest",
+        ]);
+        dockerBuildChild.stdout.on("data", async (data) => {
+          await redis.xAdd(`logs:${domain}`, "*", {
+            log: data.toString(),
+            type: "log",
+          });
+        });
+        dockerBuildChild.stderr.on("data", async (data) => {
+          await redis.xAdd(`logs:${domain}`, "*", {
+            log: data.toString(),
+            type: "error",
+          });
+          await redis.hSet(`deployments:${domain}`, {
             status: "failed",
           });
-        }
+        });
 
-        try {
-          await execPromise(
-            `docker cp ${domain}:/react-builder/repo/${outputDir} ${path.join(
-              __dirname,
-              "outputs",
+        dockerBuildChild.on("close", async() => {
+          await redis.xAdd(`logs:${domain}`, "*", {
+            log: "done with build process",
+            type: "log",
+          });
+          const dockerCopyChild = spawn("docker", [
+            "cp",
+            `${domain}:/react-builder/repo/${outputDir}`,
+            `${path.join(__dirname, "outputs", domain)}`,
+          ]);
+          dockerCopyChild.stderr.on("data", async (data) => {
+            await redis.xAdd(`logs:${domain}`, "*", {
+              log: data.toString(),
+              type: "error",
+            });
+            await redis.hSet(`deployments:${domain}`, {
+              status: "failed",
+            });
+          });
+          dockerCopyChild.on("close", async () => {
+            await redis.xAdd(`logs:${domain}`, "*", {
+              log: "uploading build",
+              type: "log",
+            });
+            await uploadFolder(
+              path.join(__dirname, "outputs", domain, outputDir),
               domain
-            )}`
-          );
-        } catch (error) {
-          console.log(error);
-        }
-        await uploadFolder(path.join(__dirname, "outputs", domain,outputDir), domain);
-        const new_url = backendURL.protocol.concat(`//${domain}.`).concat(backendURL.hostname)
-        await redis.hSet(`deployments:${domain}`, {url:new_url, status: "deployed"});
-        await execPromise(`docker container prune -f && rm -r ${path.join(__dirname,"outputs",domain)}`);
+            );
+            const new_url = backendURL.protocol
+              .concat(`//${domain}.`)
+              .concat(backendURL.hostname);
+            await redis.hSet(`deployments:${domain}`, {
+              url: new_url,
+              status: "deployed",
+            });
+            await execPromise(
+              `docker container prune -f && rm -r ${path.join(
+                __dirname,
+                "outputs",
+              )}`
+            );
+          });
+        });
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     },
     {
--- apps/react-builder-container/index.js
+++ apps/react-builder-container/index.js
@@ -1,15 +1,23 @@
-const {exec} = require('child_process')
+const {spawn} = require('child_process')
 const path = require('path')
 const githubURL = process.env.GITHUB_URL
 const buildCmd = process.env.BUILD_CMD
 const installCmd = process.env.INSTALL_CMD
-exec(`git clone ${githubURL} ${path.join(__dirname,'repo')}`,(err,stdout,stderr)=>{
-    if(err) console.log(err)
-    else{
-        console.log(stdout)
-        exec(`cd ${path.join(__dirname,'repo')} && ${installCmd} && ${buildCmd}`,(err,stdout,stderr)=>{
-            if(err) console.log(err)
-            else console.log(stdout)
-        })
-}
+const gitCloneChild = spawn("git",["clone",githubURL,"repo"],{shell : true})
+gitCloneChild.stdout.on("data",(data)=>{
+    console.log(data.toString())
+})
+
+gitCloneChild.stderr.on("data",(data)=>{
+    console.log(data.toString())
+})
+
+gitCloneChild.on("close",()=>{
+    const buildChild = spawn("cd",[path.join(__dirname,'repo'),"&&","echo",installCmd,"&&",installCmd,"&&","echo",buildCmd,"&&",buildCmd],{shell : true})
+    buildChild.stdout.on("data",(data)=>{
+        console.log(data.toString())
+    })
+    buildChild.stderr.on("data",(data)=>{
+        console.error(data.toString())
+    })
 })
