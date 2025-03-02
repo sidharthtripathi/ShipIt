cd7d67222948cff3b8d59c0ef3c2b7825be1e455
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-03-02 16:21:03 +0530

    add github repo regex

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,14 +1,27 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    url : z.string().url(),
-    outputDir : z.string().min(1),
-    buildCmd : z.string().min(1),
-    installCmd : z.string().min(1),
+  url: z
+    .string()
+    .regex(
+      /^https:\/\/github\.com\/(?!-)(?!.*--)[A-Za-z0-9-]{1,39}(?<!-)\/[A-Za-z0-9_.-]+$/,
+      {
+        message:
+          "Invalid GitHub URL format. Expected 'https://github.com/user/repo'.",
+      }
+    ),
+  outputDir: z.string().min(1),
+  buildCmd: z.string().min(1),
+  installCmd: z.string().min(1),
 
-    envs : z.array(z.object({key : z.string().min(1,"key can not be empty"), value : z.string().min(1,"value can not be empty")}))
-})
+  envs: z.array(
+    z.object({
+      key: z.string().min(1, "key can not be empty"),
+      value: z.string().min(1, "value can not be empty"),
+    })
+  ),
+});
 
 export const QueueDeploymentMessageSchema = formSchema.extend({
-    domain : z.string().min(1)
-})
+  domain: z.string().min(1),
+});
